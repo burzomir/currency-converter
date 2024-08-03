9199d2dda5fff612dc5a24696065052a14ba2eb5
burzomir <burzomirdev@gmail.com>
2024-08-04 01:08:33 +0200

    Created a separate hooks module

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { Autocomplete, Stack, TextField } from "@mui/material";
 import { CurrencyExchange } from "@mui/icons-material";
-import { useId, useSyncExternalStore } from "react";
+import { useId } from "react";
 import { NumericFormat } from "react-number-format";
+import { useSeparators } from "@/hooks";
 
 export default function Home() {
   return (
@@ -53,28 +54,3 @@ const currencies = [
   { code: "EUR", name: "Euro" },
   { code: "USD", name: "US Dollar" },
 ];
-
-function useSeparators() {
-  const language = useLanguage();
-  const formatter = Intl.NumberFormat(language);
-  const decimalSeparator = formatter
-    .formatToParts(0.1)
-    .find((part) => part.type === "decimal")?.value;
-  const thousandSeparator = formatter
-    .formatToParts(1000)
-    .find((part) => part.type === "group")?.value;
-  return { decimalSeparator, thousandSeparator };
-}
-
-function useLanguage() {
-  return useSyncExternalStore(subscribeLanguage, getLanguage);
-}
-
-function subscribeLanguage(callback: () => void) {
-  window.addEventListener("languagechange", callback);
-  return () => window.removeEventListener("languagechange", callback);
-}
-
-function getLanguage() {
-  return navigator.language;
-}
---
+++ src/hooks.ts
@@ -0,0 +1,26 @@
+import { useSyncExternalStore } from "react";
+
+export function useSeparators() {
+  const language = useLanguage();
+  const formatter = Intl.NumberFormat(language);
+  const decimalSeparator = formatter
+    .formatToParts(0.1)
+    .find((part) => part.type === "decimal")?.value;
+  const thousandSeparator = formatter
+    .formatToParts(1000)
+    .find((part) => part.type === "group")?.value;
+  return { decimalSeparator, thousandSeparator };
+}
+
+function useLanguage() {
+  return useSyncExternalStore(subscribeLanguage, getLanguage);
+}
+
+function subscribeLanguage(callback: () => void) {
+  window.addEventListener("languagechange", callback);
+  return () => window.removeEventListener("languagechange", callback);
+}
+
+function getLanguage() {
+  return navigator.language;
+}
