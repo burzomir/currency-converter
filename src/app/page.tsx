c67263f1691706166cb22632078539c10b98c02f
burzomir <burzomirdev@gmail.com>
2024-08-04 02:35:44 +0200

    Extracted converter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,7 @@
-"use client";
-import { Stack } from "@mui/material";
-import { CurrencyExchange } from "@mui/icons-material";
-import { useState } from "react";
-import CurrencySelect from "@/components/currency-select";
-import AmountInput from "@/components/amount-input";
+import Converter from "@/components/converter";
 
 export default function Home() {
-  return (
-    <Stack
-      direction={{ xs: "column", sm: "row" }}
-      spacing={3}
-      alignItems="center"
-      justifyContent="center"
-      height="100vh"
-    >
-      <CurrencyInput />
-      <CurrencyExchange fontSize="large" color="info" />
-      <CurrencyInput />
-    </Stack>
-  );
-}
-
-function CurrencyInput() {
-  const [currency, setCurrency] = useState(currencies[0]);
-  const [amount, setAmount] = useState(0);
-  return (
-    <Stack gap={3}>
-      <CurrencySelect
-        currencies={currencies}
-        value={currency}
-        onChange={setCurrency}
-      />
-      <AmountInput value={amount} onChange={setAmount} />
-    </Stack>
-  );
+  return <Converter currencies={currencies} />;
 }
 
 const currencies = [
