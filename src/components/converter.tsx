1a8923559b5d172553474bd80f5d337faf783e0d
burzomir <burzomirdev@gmail.com>
2024-08-04 15:37:02 +0200

    Extracted convert function

--- src/components/converter.tsx
+++ src/components/converter.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { Currency, CurrencyCode } from "@/types";
-import ConverterForm, { FormField, initFormState } from "./converter-form";
+import ConverterForm, {
+  FormField,
+  FormState,
+  initFormState,
+} from "./converter-form";
 import { useRef, useState } from "react";
 import { produce } from "immer";
 
@@ -13,48 +17,56 @@ export default function Converter(props: ConverterProps) {
   const initialFormState = initFormState(props.currencies[0].code);
   const [formState, setFormState] = useState(initialFormState);
   const requestIdRef = useRef<string>();
+
+  const convert = async (
+    data: Data,
+    // eslint-disable-next-line
+    apply: (value: number) => FormState
+  ) => {
+    let requestId = crypto.randomUUID();
+    requestIdRef.current = requestId;
+    const value = await requestConversion(data);
+    if (requestIdRef.current !== requestId) {
+      return;
+    }
+    const newState = apply(value);
+    setFormState(newState);
+  };
+
   const fromChange = async (from: FormField) => {
     const newState = produce(formState, (draft) => {
       draft.from = from;
     });
     setFormState(newState);
-    let requestId = crypto.randomUUID();
-    requestIdRef.current = requestId;
-    const value = await convert({
+    const data = {
       from: newState.from.currencyCode,
       to: newState.to.currencyCode,
       amount: newState.from.amount,
-    });
-    if (requestIdRef.current !== requestId) {
-      return;
-    }
-    setFormState(
+    };
+    const finalize = (value: number) =>
       produce(newState, (draft) => {
         draft.to.amount = value;
-      })
-    );
+      });
+    convert(data, finalize);
   };
+
   const toCurrencyChange = async (currencyCode: CurrencyCode) => {
     const newState = produce(formState, (draft) => {
       draft.to.currencyCode = currencyCode;
     });
     setFormState(newState);
-    let requestId = crypto.randomUUID();
-    requestIdRef.current = requestId;
-    const value = await convert({
+    const data = {
       from: newState.from.currencyCode,
       to: newState.to.currencyCode,
       amount: newState.from.amount,
-    });
-    if (requestIdRef.current !== requestId) {
-      return;
-    }
-    setFormState(
+    };
+    const finalize = (value: number) =>
       produce(newState, (draft) => {
         draft.to.amount = value;
-      })
-    );
+      });
+    convert(data, finalize);
   };
+
   const toAmountChange = async (amount: number) => {
     const newState = produce(formState, (draft) => {
       draft.to.amount = amount;
@@ -65,18 +77,13 @@ export default function Converter(props: ConverterProps) {
       to: newState.from.currencyCode,
       amount: newState.to.amount,
     };
-    let requestId = crypto.randomUUID();
-    requestIdRef.current = requestId;
-    const value = await convert(data);
-    if (requestIdRef.current !== requestId) {
-      return;
-    }
-    setFormState(
+    const finalize = (value: number) =>
       produce(newState, (draft) => {
         draft.from.amount = value;
-      })
-    );
+      });
+    convert(data, finalize);
   };
+
   return (
     <ConverterForm
       currencies={props.currencies}
@@ -88,11 +95,13 @@ export default function Converter(props: ConverterProps) {
   );
 }
 
-async function convert(data: {
+type Data = {
   from: CurrencyCode;
   to: CurrencyCode;
   amount: number;
-}): Promise<number> {
+};
+
+async function requestConversion(data: Data): Promise<number> {
   const response = await fetch("/api/convert", {
     method: "POST",
     body: JSON.stringify(data),
