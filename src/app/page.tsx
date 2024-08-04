8a8f656cca4d931e7eb93734ea9b9e9c08974f3b
burzomir <burzomirdev@gmail.com>
2024-08-04 02:55:12 +0200

    Implemented fetching of the list of currencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import Converter from "@/components/converter";
+import { Currency } from "@/types";
+import assert from "assert";
 
-export default function Home() {
+export default async function Home() {
+  const currencies = await fetchCurrencies();
   return <Converter currencies={currencies} />;
 }
 
-const currencies = [
-  { code: "EUR", name: "Euro" },
-  { code: "USD", name: "US Dollar" },
-];
+async function fetchCurrencies() {
+  const url = new URL("https://api.currencybeacon.com/v1/currencies");
+  assert(
+    process.env.CURRENCY_BEACON_API_KEY,
+    "CURRENCY_BEACON_API_KEY is not set"
+  );
+  url.searchParams.append("api_key", process.env.CURRENCY_BEACON_API_KEY);
+  const response = await fetch(url, { next: { revalidate: 60 * 60 * 24 } });
+  const json = await response.json();
+  const currencies: Currency[] = json.response.map((data: any) => ({
+    code: data.short_code,
+    name: data.name,
+  }));
+  return currencies;
+}
